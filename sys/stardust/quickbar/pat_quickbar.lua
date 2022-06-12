dd7255222353604d1f9879d9362b2c1a6773f049
patman <27874300+patmanf@users.noreply.github.com>
2022-06-12 16:43:00 -0500

    make the tooltips good

--- sys/stardust/quickbar/pat_quickbar.lua
+++ sys/stardust/quickbar/pat_quickbar.lua
@@ -161,35 +161,7 @@ local function buildList()
   end
 end
 
-local function compactTooltip(screenPosition)
-  for widgetName, label in pairs(compactLabels) do
-    if widget.inMember(widgetName, screenPosition) then
-      local tooltip = config.getParameter("tooltipLayout")
-      sb.logInfo(sb.printJson(tooltip))
-      
-      tooltip.text.value = label
-      if not tooltip.text.value or #tooltip.text.value == 0 then return end
-      
-      -- , yeah
-      local text = string.gsub(tooltip.text.value, "(^.-;)", "") --remove colors
-      local _,smalls = string.gsub(text, "[il.,:;'| ]", "") --small characters . i guess
-      local _,m = string.gsub(text, "[MmWw]", "") --m
-      local size = math.max(tooltip.background.size[1], math.ceil((#text + (m * 1.4) - (smalls * 0.6)) * 7.1))
-      
-      tooltip.background.size[1] = size
-      tooltip.text.position[1] = size / 2
-      tooltip.bg.fileBody = tooltip.bg.fileBody.."?scale="..size..";1"
-      
-      return tooltip
-    end
-  end
-end
-
 function init()
-	if config.getParameter("compacted") == true then
-    createTooltip = compactTooltip
-	end
-	
   buildList()
 end
 
